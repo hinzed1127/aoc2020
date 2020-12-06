c8a1b79083faf333ba1bd9d7eeab27ff812f9fc1
Dan Hinze <dan.hinze@adhocteam.us>
2020-12-06 12:47:53 -0600

    Combine both parts in the same method; refactor switch statement to regex mapping

--- 04/main.rb
+++ 04/main.rb
@@ -19,8 +19,28 @@
 file = File.open('input.txt')
 passports = file.readlines.map(&:chomp)
 
-def pt_1(passports)
+def count_valid_passports(passports, validate = false)
     required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    validations_regex = {
+        #byr (Birth Year) - four digits; at least 1920 and at most 2002.
+        'byr' => /^(19[2-9]\d)|(200[0-2])$/,
+        #iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+        'iyr' => /^20((1\d)|(20))$/,
+        #eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+        'eyr' => /^20((2\d)|(30))$/,
+        #hgt (Height) - a number followed by either cm or in:
+        #   If cm, the number must be at least 150 and at most 193.
+        #   If in, the number must be at least 59 and at most 76.
+        'hgt' => /^((59)|(6\d)|(7[0-6]))in|(1(([5-8]\d)|(9[0-3]))cm)$/,
+        #hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        'hcl' => /^#[0-9a-f]{6}$/,
+        #ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        'ecl' => /^(amb|blu|brn|gry|grn|hzl|oth)$/,
+        #pid (Passport ID) - a nine-digit number, including leading zeroes.
+        'pid' => /^\d{9}$/,
+        #cid (Country ID) - ignored, missing or not.
+        'cid' => /./,
+    }
 
     valid_passports = 0
     current_passport = {}
@@ -34,10 +54,14 @@ def pt_1(passports)
             end
         end
 
-        if line == '' || idx == passports.length - 1
+        # second condition accounts for last entry being stripped of whitespace by Ruby
+        if line == '' || idx == passports.length - 1 
             valid_passport = true
+            validation = /./ # automatically match all when validation is false
             required_fields.each do |field|
-                if current_passport[field].nil?
+                validation = validations_regex[field] unless !validate
+
+                if current_passport[field].nil? || !current_passport[field].match(validation)
                     valid_passport = false
                     break
                 end
@@ -52,10 +76,7 @@ def pt_1(passports)
     p valid_passports
 end
 
-pt_1(passports)
-# pt_1(passports[0..100])
-
-# wrong answers: 250
-# wrong answers: 238
-
-# That's not the right answer. Curiously, it's the right answer for someone else; you might be logged in to the wrong account or just unlucky. In any case, you need to be using your puzzle input. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 250.) [Return to Day 4]
+# Part 1, no validations on values
+count_valid_passports(passports)
+# Part 2, include validations
+count_valid_passports(passports, true)
