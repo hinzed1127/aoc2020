9fe7e79172c65928a25413df87c882c0bf3b7212
Dan Hinze <dan.hinze@adhocteam.us>
2020-12-08 10:48:55 -0600

    Day 7 pt 2

--- 07/main.rb
+++ 07/main.rb
@@ -25,15 +25,6 @@ class Graph
     end
 end
 
-# file = File.open('test_input.txt')
-file = File.open('input.txt')
-rules = file.readlines.map{ |line| line.split('contain').map(&:strip) }
-rules.each do |line|
-    line[0].delete_suffix!('s')
-    line[1].delete_suffix!('.')
-    line[1] = line[1].split(',').map{ |bag| bag.strip.delete_suffix('s') }
-end
-
 def build_graph(rules)
     graph = Graph.new
     paths_to_gold = 0
@@ -55,37 +46,11 @@ def build_graph(rules)
     graph
 end
 
-def nvm
-    graph.vertices.values.each do |vertex|
-        if vertex.name == 'shiny gold bag'
-            next
-        end
-
-        vertex.edges.each do |edge|
-            current_vertex_name = edge
-            if edge == 'shiny gold bag'
-                paths_to_gold += 1
-                break
-            end
-
-            while !graph.vertices[edge.name].edges.nil?
-                current_vertex_name = graph.vertices[edge.name].name
-            end
-        end
-    end
-end
-
 def traverse_contains_gold?(graph, vertex)
-    puts "#{vertex.name}: #{vertex.edges}"
     holds_gold = false
 
-    # if vertex.name == "shiny gold bag"
-    #     return 1
-    # end
-
     if vertex.edges.include?("shiny gold bag")
-        # pp vertex
-        return holds_gold = true
+        holds_gold = true
     else
         vertex.edges.each do |edge|
             holds_gold = traverse_contains_gold?(graph, graph.vertices[edge])
@@ -100,17 +65,51 @@ end
 
 def pt_1(graph)
     total = 0
-    # graph.vertices.each{ |name, vertex| total += traverse(graph, vertex) }
-    graph.vertices.each do |name, vertex| 
-        total += traverse_contains_gold?(graph, vertex) ? 1 : 0
-        puts "Total: #{total}"
-    end 
+    graph.vertices.each{ |name, vertex| total += traverse_contains_gold?(graph, vertex) ? 1 : 0 }
+        
     p total
 end
 
+def traverse_weighted(graph, vertex, weight)
+    return weight unless vertex.edges.length > 0
+    
+    sum = 0
+
+    vertex.edges.each_with_index do |edge, idx| 
+        sum += traverse_weighted(graph, graph.vertices[edge], vertex.weights[idx] )
+    end
+
+    return weight + (weight * sum)
+end
+
+def pt_2(graph, gold_bag_vertex)
+    total_bags = 0
+
+    gold_bag_vertex.edges.each_with_index do |vertex, idx|
+        total_bags += traverse_weighted(graph, graph.vertices[vertex], gold_bag_vertex.weights[idx])
+    end
+
+    p total_bags
+end
+
+file = File.open('test_input_pt_2.txt') # => 126
+file = File.open('test_input.txt') # => 32
+file = File.open('input.txt') 
+
+rules = file.readlines.map{ |line| line.split('contain').map(&:strip) }
+rules.each do |line|
+    line[0].delete_suffix!('s')
+    line[1].delete_suffix!('.')
+    line[1] = line[1].split(',').map{ |bag| bag.strip.delete_suffix('s') }
+end
+
 graph = build_graph(rules)
-# delete gold bag to avoid off-by-one counting error 
-graph.vertices.delete('shiny gold bag')
+# delete gold bag to avoid off-by-one counting error in pt 1s EDIT: not necessary
+# Q: are there scenarios in which it would contribute to an off-by-one error"
+gold_bag_vertex = graph.vertices['shiny gold bag']
+
 pt_1(graph)
+pt_2(graph, gold_bag_vertex)
+
 
 
